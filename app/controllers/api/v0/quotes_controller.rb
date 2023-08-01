20586e76e14fc4d232981f098437d20fe96cac3f
raptorf1 <raptor_f1@hotmail.com>
2023-08-01 16:26:10 +0300

    Adds sad path when all params are missing

--- app/controllers/api/v0/quotes_controller.rb
+++ app/controllers/api/v0/quotes_controller.rb
@@ -1,5 +1,12 @@
 class Api::V0::QuotesController < ApplicationController
   def create
+    if params[:from].nil? || params[:to].nil? || params[:vehicle].nil?
+      render json: {
+               errors: ["Both from and to postcodes and a vehicle type needed to calculate a cost!"]
+             },
+             status: 400 and return
+    end
+
     render json: {
              from: params[:from],
              to: params[:to],
